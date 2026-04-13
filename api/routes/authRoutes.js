5578a3185ea61394665be2d9873be5d18bc9ddd5
Tanvi <tanvi29virani@gmail.com>
2026-04-13 21:57:37 +0530

    Removing redundant api routes of product from authRoutes.js

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,10 +1,6 @@
 import express from "express";
 import {
-  // addProduct,
-  // getProducts,
-  // getProduct,
-  // updateProduct,
-  // deleteProduct,
+
   registerUser,
   loginUser
 } from "../controllers/authController.js";
@@ -16,13 +12,7 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 
-// Product Routes
-// router.post("/products", addProduct);
-// router.get("/products", getProducts);
-// router.get("/products/:id", getProduct);
-// router.put("/products/:id", updateProduct);
-// router.delete("/products/:id", deleteProduct);
-// Protected route
+
 router.get("/dashboard", verifyToken, (req, res) => {
   res.json({
     message: "Welcome",
