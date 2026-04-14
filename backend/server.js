bfcc57ae43fa8cc65058d41725bd669f44b2df02
Tanvi <tanvi29virani@gmail.com>
2026-04-14 08:28:00 +0530

    routes changed to routes_admin_p

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import authRoutes from "./routes/authRoutes.js";
-import productRoutes from "./routes/productRoutes.js";
-import masterRoutes from "./routes/masterRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
+import admin_authRoutes from "./routes_admin_p/authRoutes.js";
+import admin_productRoutes from "./routes_admin_p/productRoutes.js";
+import admin_masterRoutes from "./routes_admin_p/masterRoutes.js";
+import admin_userRoutes from "./routes_admin_p/userRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -13,10 +13,10 @@ app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
-app.use("/api", authRoutes);
-app.use("/api/master", masterRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/user", userRoutes);
+app.use("/api", admin_authRoutes);
+app.use("/api/master", admin_masterRoutes);
+app.use("/api/products", admin_productRoutes);
+app.use("/api/user", admin_userRoutes);
 
 app.get("/", (req, res) => res.send("API Running"));
 
